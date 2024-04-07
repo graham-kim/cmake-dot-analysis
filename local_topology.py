f54218e6c490e489d13293fbc756af6564b57a62
Graham Kim <graham.kim@cantab.net>
2024-04-07 15:24:00 +0100

    Script to print outbound neighbhours
    A simple proof of concept

---
+++ local_topology.py
@@ -0,0 +1,30 @@
+import typing as tp
+import argparse
+import networkx as nx
+
+def setup_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_filename', help="Input filename")
+    parser.add_argument('--target', help="Node to focus analysis on")
+    return parser
+
+def get_immediate_child_nodes(inG: nx.DiGraph, target_node: str):
+    outG = nx.DiGraph()
+    outG.add_node(target_node, shape='box')
+    child_nodes = list(nx.neighbors(inG, target_node))
+    for child_node in child_nodes:
+        outG.add_node(child_node)
+        outG.add_edge(target_node, child_node)
+
+    dot_prog = None if len(child_nodes) < 4 else "twopi"
+    nx.nx_pydot.to_pydot(outG).write_jpg("out.jpg", prog=dot_prog)
+
+if __name__ == '__main__':
+    args = setup_parser().parse_args()
+    inG = nx.DiGraph(nx.nx_pydot.read_dot(args.input_filename))
+
+    if args.target is not None:
+        get_immediate_child_nodes(inG, args.target)
+
+    
+
